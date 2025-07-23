0c0d72d09038092a9840b6501f0d4133e401ef59
seowon <seowon1112@gmail.com>
2025-07-23 16:01:00 +0900

    init

---
+++ src/main/java/com/project/handongjudge/HandongjudgeApplication.java
@@ -0,0 +1,13 @@
+package com.project.handongjudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandongjudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandongjudgeApplication.class, args);
+	}
+
+}
