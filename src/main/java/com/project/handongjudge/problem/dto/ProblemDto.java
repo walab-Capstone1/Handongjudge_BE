d9ec242e8f148c867d684a3537ce419ff1825d1a
seowon <seowon1112@gmail.com>
2025-08-07 04:07:41 +0900

    AssignmentProblem @jsonIgnore add, ProblemDTO.java Long domjudgeProblemId -> String domjudgeProblemId

--- src/main/java/com/project/handongjudge/problem/dto/ProblemDto.java
+++ src/main/java/com/project/handongjudge/problem/dto/ProblemDto.java
@@ -12,6 +12,6 @@ public class ProblemDto {
     private String title;
     private String description;
     private String difficulty;
-    private Long domjudgeProblemId;
+    private String domjudgeProblemId;
     private LocalDateTime createdAt;
 } 
