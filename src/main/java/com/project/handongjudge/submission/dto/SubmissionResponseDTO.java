7f68935e91a050079d3ac542f32e3578f3511d68
Diggydogg <22000471@handong.edu>
2025-08-11 23:49:15 +0900

    feat: SubmissionAuthDTO 는 실제 프론트 코드 제출을 위한 DTO, (no userId required) SubmssionResponseDTO, RequestDTO 반환값 간소화 및 리팩

--- src/main/java/com/project/handongjudge/submission/dto/SubmissionResponseDTO.java
+++ src/main/java/com/project/handongjudge/submission/dto/SubmissionResponseDTO.java
@@ -8,13 +8,13 @@ import java.time.LocalDateTime;
 @Setter
 @Builder
 public class SubmissionResponseDTO {
-    private Long id;
+    
     private Long problemId;
-    private Long userId;
+    
     private Long sectionId;
     private String language;
     private String submissionId;
     private String result;      // 여기에 judgement_type_id ("AC", "WA", ...) 들어가야 함
-    private String code;
+    
     private LocalDateTime submittedAt;
 }
